135e1ced74c3470295f08955020db287bcd01f48
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-05-21 23:44:12 -0300

    feat: Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { NavbarComponent } from './Pages/Partials/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
